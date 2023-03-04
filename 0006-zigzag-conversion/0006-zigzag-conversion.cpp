8d542cc6bf047eb00a034328995d77b09536a1b7
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-04 15:39:47 +0900

    Time: 3 ms (98.45%), Space: 8.3 MB (76.38%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,44 +1,28 @@
-#define MAX_N 1'001
-
 class Solution {
 public:
     string convert(string s, int numRows) {
         if (numRows == 1) {
             return s;
         }
+        
         int len = s.size();
-        char board[MAX_N][MAX_N] = { NULL, };
-        int curX = 0;
-        int curY = 0;
-        int direction = 0;
-        for (int i = 0; i < len; i++) {
-            board[curY][curX] = s[i];
-            if (direction == 0) {
-                if (curY == numRows - 1) {
-                    direction = 1;
-                    curX += 1;
-                    curY -= 1;
-                } else {
-                    curY += 1;
-                }
-            } else {
-                if (curY == 0) {
-                    direction = 0;
-                    curY += 1;
-                } else {
-                    curX += 1;
-                    curY -= 1;
-                }
-            }
-        }
-
+        int rowIdx = 0;
+        int sectionLen = 2 * (numRows - 1);
         string ret = "";
-        for (int i = 0; i < MAX_N; i++) {
-            for (int j = 0; j < MAX_N; j++) {
-                if (board[i][j] != NULL) {
-                    ret += board[i][j]; 
+        for (int row = 0; row < numRows; row++) {
+            int idx = row;
+            while (idx < len) {
+                ret += s[idx];
+                idx += sectionLen;
+                if (row != 0 && row != numRows - 1) {
+                    idx -= 2 * row;
+                    if (idx < len) {
+                        ret += s[idx];
+                    }
+                    idx += 2 * row;
                 }
             }
+            
         }
         return ret;
     }
