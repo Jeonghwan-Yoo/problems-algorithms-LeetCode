723b4eff1b21c25f61780bcd06c951f4f9cb1039
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-05 22:57:36 +0900

    Time: 4 ms (60.17%), Space: 6.9 MB (92.90%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long num = 0;
+        int num = 0;
         int sign = 1;
         int idx = 0;
         int len = s.size();
-        while (s[idx] == ' ') {
+        
+        while (idx < len && s[idx] == ' ') {
             idx++;
         }
+        
         if (s[idx] == '-') {
             sign = -1;
             idx++;
         } else if (s[idx] == '+') {
             idx++;
         }
-        while (idx < len) {
-            if (s[idx] >= '0' && s[idx] <= '9') {
-                num = 10 * num + s[idx] - '0';
-                idx++;
-                if (num * sign > INT_MAX) {
+        
+        int intMax10 = INT_MAX / 10;
+        while (idx < len && s[idx] >= '0' && s[idx] <= '9') {
+            if (num > intMax10 || (num == intMax10 && s[idx] > '7')) {
+                if (sign == 1) {
                     return INT_MAX;
-                } else if (num * sign < INT_MIN) {
+                } else {
                     return INT_MIN;
                 }
-            } else {
-                break;
             }
+            num = 10 * num + (s[idx] - '0');
+            idx++;
         }
         
         return num * sign;
