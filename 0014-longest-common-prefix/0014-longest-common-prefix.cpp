b02d1761dbc1d507eab97a6e9cfe9a5701ff5690
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-09 00:47:38 +0900

    Time: 12 ms (10.02%), Space: 12.4 MB (9.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        if (n == 0) {
-            return "";
+        return longestCommonPrefix(strs, 0, strs.size() - 1);
+    }
+    
+    string longestCommonPrefix(vector<string>& strs, int left, int right) {
+        if (left == right) {
+            return strs[left]; 
         }
         
-        string prefix = strs[0];
-        for (int i = 0; i < prefix.size(); i++) {
-            char c = prefix[i];
-            for (int j = 0; j < n; j++) {
-                if (i == strs[j].size() || strs[j][i] != c) {
-                    return prefix.substr(0, i);
-                }
+        int mid = (left + right) / 2;
+        string lcpLeft = longestCommonPrefix(strs, left, mid);
+        string lcpRight = longestCommonPrefix(strs, mid + 1, right);
+        
+        return getCommonPrefix(lcpLeft, lcpRight);
+    }
+    
+    string getCommonPrefix(string lcpLeft, string lcpRight) {
+        int minLen = min(lcpLeft.size(), lcpRight.size());
+        for (int i = 0; i < minLen; i++) {
+            if (lcpLeft[i] != lcpRight[i]) {
+                return lcpLeft.substr(0, i);
             }
         }
-        
-        return prefix;
+        return lcpLeft.substr(0, minLen);
     }
 };
