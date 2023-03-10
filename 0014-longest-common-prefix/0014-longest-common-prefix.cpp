504d42dff6dd5147ef99d8a5e96b932c4322d4e6
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-11 02:11:43 +0900

    Time: 8 ms (30.03%), Space: 9.9 MB (11.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,36 +1,62 @@
 class Solution {
 public:
+    class TrieNode {
+    public:
+        bool isEnd;
+        unordered_map<char, TrieNode*> children;
+        TrieNode() {
+            isEnd = false;
+        }
+    };
+    class Trie {
+    public:
+        TrieNode* root;
+        Trie() {
+            root = new TrieNode();
+        }
+        void insert(string word) {
+            int len = word.size();
+            TrieNode* cur = root;
+            for (char c: word) {
+                if (cur->children.find(c) == cur->children.end()) {
+                    cur->children[c] = new TrieNode();
+                }
+                cur = cur->children[c];
+            }
+            cur->isEnd = true;
+        }
+        bool search(string word) {
+            int len = word.size();
+            TrieNode* cur = root;
+            for (char c: word) {
+                if (cur->children.find(c) == cur->children.end()) {
+                    return false;
+                }
+                cur = cur->children[c];
+            }
+            return cur->isEnd;
+        }
+        string longestCommonPrefix(string word) {
+            int len = word.size();
+            TrieNode* cur = root;
+            for (int i = 0; i < len; i++) {
+                char c = word[i];
+                if (cur->children.find(c) != cur->children.end() && cur->children.size() == 1 && cur->isEnd == false) {
+                    cur = cur->children[c];
+                } else {
+                    return word.substr(0, i);
+                }
+            }
+            return word;
+        }
+    };
+    
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
-        
-        int minLen = strs[0].size();
-        for (int i = 1; i < n; i++) {
-            minLen = min(minLen, (int)strs[i].size());
+        Trie* trie = new Trie();
+        for (int i = 0; i < n; i++) {
+            trie->insert(strs[i]);
         }
-        
-        int left = 0;
-        int right = minLen;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isCommonPrefix(strs, mid)) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        int len = (left + right) / 2;
-        return strs[0].substr(0, len);
-    }
-    
-    bool isCommonPrefix(vector<string>& strs, int len) {
-        int n = strs.size();
-        
-        string prefix = strs[0].substr(0, len);
-        for (int i = 1; i < n; i++) {
-            if (strs[i].find(prefix) != 0) {
-                return false;
-            }
-        }
-        return true;
+        return trie->longestCommonPrefix(strs[0]);
     }
 };
