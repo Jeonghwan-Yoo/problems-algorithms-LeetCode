af698a480220697174d6c8c82d353afb12205052
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-12 18:09:02 +0900

    Time: 148 ms (77.34%), Space: 24.1 MB (82.86%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,21 +4,35 @@ public:
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        unordered_map<int, int> um;
-        for (int i = 0; i < n; i++) {
-            um[nums[i]] = i;
-        }
         
         vector<vector<int>> ret;
-        for (int first = 0; first < n; first++) {
-            for (int second = first + 1; second < n; second++) {
-                int thirdValue = -(nums[first] + nums[second]);
-                if (um.count(thirdValue) > 0 && um[thirdValue] > second) {
-                    ret.emplace_back(vector<int>{ nums[first], nums[second], thirdValue });
+        int first = 0;
+        while (first < n - 2) {
+            int second = first + 1;
+            int third = n - 1;
+            while (second < third) {
+                int sum = nums[first] + nums[second] + nums[third];
+                if (sum == 0) {
+                    ret.emplace_back(vector<int>{ nums[first], nums[second], nums[third] });
+                    while (second + 1 < third && nums[second] == nums[second + 1]) {
+                        second++;    
+                    }
+                    while (second < third - 1 && nums[third] == nums[third - 1]) {
+                        third--;    
+                    }
+                    second++;
+                    third--;
                 }
-                second = um[nums[second]];
+                else if (sum < 0) {
+                    second++;
+                } else {
+                    third--;
+                }
             }
-            first = um[nums[first]];
+            while (first + 1 < n - 2 && nums[first] == nums[first + 1]) {
+                first++;
+            }
+            first++;
         }
         
         return ret;
