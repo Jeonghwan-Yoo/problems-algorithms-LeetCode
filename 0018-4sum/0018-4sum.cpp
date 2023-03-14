ba13b92ebe68f4491a788770dce5abc302cb17dc
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-15 00:42:25 +0900

    Time: 100 ms (50.43%), Space: 13.2 MB (71.78%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,27 +7,35 @@ public:
         int n = nums.size();
         for (int first = 0; first < n - 3; first++) {
             for (int second = first + 1; second < n - 2; second++) {
-                for (int third = second + 1; third < n - 1; third++) {
-                    for (int fourth = third + 1; fourth < n; fourth++) {
-                        if ((long long)nums[first] + nums[second] + nums[third] + nums[fourth] == target) {
-                            ret.emplace_back(vector<int>{ nums[first], nums[second], nums[third], nums[fourth] });
-                            while (fourth + 1 < n && nums[fourth] == nums[fourth + 1]) {
-                                fourth++;
-                            }
-                        }
-                        while (third + 1 < n && nums[third] == nums[third + 1]) {
+                int third = second + 1;
+                int fourth = n - 1;
+                while (third < fourth) {
+                    long long sum = (long long)nums[first] + nums[second] + nums[third] + nums[fourth];
+                    if (sum == target) {
+                        ret.emplace_back(vector<int>{ nums[first], nums[second], nums[third], nums[fourth] });
+                        while (third + 1 < fourth && nums[third] == nums[third + 1]) {
                             third++;
                         }
-                    }
-                    while (second + 1 < n && nums[second] == nums[second + 1]) {
-                        second++;
+                        third++;
+                        while (third < fourth - 1 && nums[fourth] == nums[fourth - 1]) {
+                            fourth--;
+                        }
+                        fourth--;
+                    } else if (sum < target) {
+                        third++;
+                    } else {
+                        fourth--;
                     }
                 }
-                while (first + 1 < n && nums[first] == nums[first + 1]) {
-                    first++;
+                while (second + 1 < n - 2 && nums[second] == nums[second + 1]) {
+                    second++;
                 }
             }
+            while (first + 1 < n - 3 && nums[first] == nums[first + 1]) {
+                first++;
+            }
         }
+        
         return ret;
     }
 };
