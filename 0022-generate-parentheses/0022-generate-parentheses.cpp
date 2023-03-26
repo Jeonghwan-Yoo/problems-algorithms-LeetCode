63e8fb61ecdad47c616e0f70c506705e8598535e
Jeonghwan-Yoo <54882564+Jeonghwan-Yoo@users.noreply.github.com>
2023-03-26 23:55:20 +0900

    Time: 56 ms (5.07%), Space: 53.3 MB (5.02%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> ret;
-        
-        function<void(int, int, string)> generate = [&](int open, int close, string cur) {
-            if (open == 0 && close == 0) {
-                ret.emplace_back(cur);
-                return;
-            }
-            if (open > 0) {
-                generate(open - 1, close, cur + "(");    
-            }
-            if (close > 0 && open < close) {
-                generate(open, close - 1, cur + ")");    
-            }
-        };
-        generate(n, n, "");
+        makeAllParenthesis(n, n, "", ret);
         
         return ret;
+    }
+    
+    void makeAllParenthesis(int open, int close, string cur, vector<string>& parenthsis) {
+        if (open == 0 && close == 0) {
+            if (isValid(cur)) {
+                parenthsis.emplace_back(cur);
+            }
+            return;
+        }
+        if (open > 0) {
+           makeAllParenthesis(open - 1, close, cur + "(", parenthsis); 
+        }
+        if (close > 0) {
+            makeAllParenthesis(open, close - 1, cur + ")", parenthsis);
+        }
+    }
+    
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c: s) {
+            if (c == ')') {
+                if (st.empty() || c != st.top()) {
+                    return false;
+                }
+                st.pop();
+            } else if (c == '(') {
+                st.push(')');
+            }
+        }
+        return st.empty();
     }
 };
